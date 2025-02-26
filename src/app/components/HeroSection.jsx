8522e210c06ef4fd331e4676d9ce91095c505ca5
Pedro-hashm <pedrosadultos@hotmail.com>
2025-02-25 21:58:20 -0300

    NavBar

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,6 +1,8 @@
+"use client";
 import React from "react";
 import "../styles/HeroSection.css";
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
@@ -9,16 +11,34 @@ const HeroSection = () => {
 
             <div className="col-span-7 place-self-center text-center sm:text-left mb-4 mr-4">
                 <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold"> 
-                    Hello, i'm Pedro
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 via-pink-400 to-purple-600 ">
+                        Hello, i'm{" "}
+                    </span>
+                    <br></br>
+                    <TypeAnimation
+                        sequence={[
+                        'Pedro',
+                        3000, 
+                        'Full Stack Developer',
+                        3000,
+                        'Web Developer',
+                        3000,
+                        'UI/UX Designer',
+                        3000,
+                        ]}
+                        wrapper="span"
+                        speed={50}
+                        repeat={Infinity}
+                    />
                 </h1>
                 <p className="text-slate-400 text-base sm:text-lg lg:text-xl">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.                </p>
                 <div>
-                    <button className="text-black font-bold rounded-full w-full sm:fit bg-[#ffffff] hover:bg-slate-200 px-4 py-4 mr-2 mt-4">
+                    <button className="text-white font-bold rounded-full w-full sm:fit bg-gradient-to-br from-yellow-500 via-pink-400 to-purple-600 px-4 py-4 mr-2 mt-4">
                         Hire me
                     </button>
-                    <button className="text-white font-bold rounded-full w-full sm:fit bg-transparent hover:bg-slate-800 border px-4 py-4 mr-2 mt-4">
-                        Download CV
+                    <button className="text-white font-bold rounded-full w-full sm:fit bg-gradient-to-br from-yellow-500 via-pink-400 to-purple-600 px-1 py-1 mr-2 mt-4">
+                       <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-4 py-4"> Download CV </span>
                     </button>
                 </div>
             </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
+import NavBar from "./components/NavBar";
+import NavLink from "./components/NavLink";
 
 export default function Home() {
   return (
       <main className="flex min-h-screen flex-col bg-[#121212]">
-
-        <div className="container mx-auto px-12 py-4">
+        <div>
+          <NavBar/>
+        </div>
+        <div className="container mt-24 mx-auto px-12 py-4">
           <HeroSection/>
         </div>
       </main>
