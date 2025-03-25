6ebf14c45d6502f437a69dc2fc33cfe99a28def0
Pedro-hashm <pedrosadultos@hotmail.com>
2025-03-25 00:10:54 -0300

    Project Section Progress

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -1,17 +1,39 @@
+"use client";
 import React from 'react'
 import Image from 'next/image';
+import { CodeBracketIcon } from '@heroicons/react/24/solid';
+import { EyeIcon } from '@heroicons/react/24/solid';
 
-const ProjectCard = ({ imageUrl, title, description }) => {
+const ProjectCard = ({ imageUrl, imgTitle, title, description }) => {
     return (
         <div className="flex flex-col p-2">
 
-            <div>
+            <div className="relative group w-[400px] h-[300px] overflow-hidden rounded-lg">
                 <Image
                     src={imageUrl}
+                    alt={imgTitle}
                     width={400}
                     height={300}
+                    className="w-full h-full object-cover"
                 >
                 </Image>
+
+                <div className="flex flex-row absolute inset-0 bg-black bg-opacity-0 gap-2 place-content-center hover:bg-opacity-70 transition duration-300 group/card">
+                    <button>
+                        <div 
+                            className="group/btn p-3 border-2 border-[#c0c0c0] rounded-full opacity-0 group-hover:opacity-100 transition duration-300 hover:border-white"
+                            onClick={() => window.open("https://github.com/Pedro-hashm", "_blank")}
+                            >
+                            <CodeBracketIcon className="group/btn w-8 h-8 text-[#c0c0c0] group-hover/btn:text-white transition duration-300"/>
+                        </div>
+                    </button>
+                    <button>
+                        <div className="group/btn p-3 border-2 border-[#c0c0c0] rounded-full opacity-0 group-hover:opacity-100 transition duration-300 hover:border-white">
+                            <EyeIcon className="group/btn w-8 h-8 text-[#c0c0c0] group-hover/btn:text-white transition duration-300"/>
+                        </div>
+                    </button>
+                </div>
+
             </div>
 
             <div className="mt-2 ml-3">
--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -1,45 +1,87 @@
+"use client";
 import React from 'react'
 import ProjectCard from './ProjectCard'
+import { useState } from 'react';
 
-function ProjectSection() {
+/* Dados dos Projetos (Pra adicionar um projeto é só modificar aqui) */
+const projects = [
+  {
+    imageUrl: "https://abovepromotions.com/wp-content/uploads/400x300-ph.jpg",
+    title: "Projeto 1 (Feito)",
+    description: "Descrição do projeto 1",
+    alt: "Imagem do projeto 1",
+    tags: ["Completed"]
+  },
+  {
+    imageUrl: "https://abovepromotions.com/wp-content/uploads/400x300-ph.jpg",
+    title: "Projeto 2 (Ativo)",
+    description: "Descrição do projeto 2",
+    alt: "Imagem do projeto 2",
+    tags: ["Active"]
+  },
+  {
+    imageUrl: "https://abovepromotions.com/wp-content/uploads/400x300-ph.jpg",
+    title: "Projeto 3 (Ativo)",
+    description: "Descrição do projeto 3",
+    alt: "Imagem do projeto 3",
+    tags: ["Active"]
+  },
+  {
+    imageUrl: "https://abovepromotions.com/wp-content/uploads/400x300-ph.jpg",
+    title: "Projeto 4 (Feito)",
+    description: "Descrição do projeto 4",
+    alt: "Imagem do projeto 4",
+    tags: ["Completed"]
+  }
+];
+
+const ProjectSection = () => {
+
+  const [activeTag, setActiveTag] = useState("All");
+  
+  const handleTabChange = (tag) => {
+    setActiveTag(tag);
+  }
+
+  const filteredProjects = activeTag === "All"
+    ? projects
+    : projects.filter(project => project.tags.includes(activeTag));
+
   return (
     <section>
 
-        <div className="md:grid md:grid-cols-1 sm:py-16 grid place-items-center px-4 mt-2 mb-4"> 
-          <h1 className="text-4xl mb-4 sm:text-3xl lg:text-4xl font-extrabold ">Meus Projetos</h1>
-        </div>
+      <div className="md:grid md:grid-cols-1 grid place-items-center px-4 mt-2"> 
+        <h1 className="text-4xl mb-4 sm:text-3xl lg:text-4xl font-extrabold ">Meus Projetos</h1>
+      </div>
+      
+      <div className="flex flex-row justify-center gap-3 px-4 mt-6 mb-4">
+        {["All", "Active", "Completed"].map(tag => (
+          <button 
+            key={tag}
+            className={`border-2 pt-2 pb-2 pr-5 pl-5 border-[#4e5568] rounded-full hover:border-white ${
+              activeTag === tag ? "border-purple-500 text-white" : "text-[#c0c0c0]"
+            }`}
+            onClick={() => setActiveTag(tag)}
+          >
+            {tag}
+          </button>
+        ))}
+      </div>
 
-        <div className="flex flex-wrap justify-center gap-6">
+      <div className="flex flex-wrap justify-center gap-6">
+        {filteredProjects.map((project, index) => (
           <ProjectCard
-            imageUrl="https://abovepromotions.com/wp-content/uploads/400x300-ph.jpg"
-            title="Projeto 1"
-            description="Descrição do projeto 1"
-            >
-          </ProjectCard>
-          <ProjectCard
-            imageUrl="https://abovepromotions.com/wp-content/uploads/400x300-ph.jpg"
-            title="Projeto 1"
-            description="Descrição do projeto 1"
-            >
-          </ProjectCard>
-          <ProjectCard
-            imageUrl="https://abovepromotions.com/wp-content/uploads/400x300-ph.jpg"
-            title="Projeto 1"
-            description="Descrição do projeto 1"
-            >
-          </ProjectCard>
-          <ProjectCard
-            imageUrl="https://abovepromotions.com/wp-content/uploads/400x300-ph.jpg"
-            title="Projeto 1"
-            description="Descrição do projeto 1"
-            >
-          </ProjectCard>
-          
-  
-        </div>
+            key={index}
+            imageUrl={project.imageUrl}
+            title={project.title}
+            description={project.description}
+            imgTitle={project.alt}
+          />
+        ))}
+      </div>
 
     </section>
-  )
+  );
 }
 
 export default ProjectSection
